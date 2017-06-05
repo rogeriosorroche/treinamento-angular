3ba6890e1ab76a0fe640cd2dc7d2e9011435b553
Rogério Sorroche <rogerio.sorroche@gmail.com>
2017-06-05 12:32:42 -0300

    Exercicio 2

---
+++ caelumpic/src/app/foto/foto.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'foto',
+    templateUrl: './foto.component.html',
+    styleUrls: ['./foto.component.css']
+})
+export class FotoComponent {
+    @Input() titulo: string;
+    @Input() url: string;
+}
